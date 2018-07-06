f0750938b2704a2638ce5a101ccf6cb97a32f412
Pavel <tpmiczzi@i.ua>
2018-07-06 17:15:48 +0300

    second test

--- src/main/java/com/java8/demo/model/GitTest.java
+++ src/main/java/com/java8/demo/model/GitTest.java
@@ -5,4 +5,30 @@ public class GitTest {
     private String git;
 
     private String nextChange;
+
+    private String seckkndChange;
+
+    public String getGit() {
+        return git;
+    }
+
+    public void setGit(String git) {
+        this.git = git;
+    }
+
+    public String getNextChange() {
+        return nextChange;
+    }
+
+    public void setNextChange(String nextChange) {
+        this.nextChange = nextChange;
+    }
+
+    public String getSeckkndChange() {
+        return seckkndChange;
+    }
+
+    public void setSeckkndChange(String seckkndChange) {
+        this.seckkndChange = seckkndChange;
+    }
 }
